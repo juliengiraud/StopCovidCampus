60b48f197f6fd59e3bbfd0f7277bf4d397662376
jeremy <jeremy.thomas@etu.univ-lyon1.fr>
2020-11-03 15:29:42 +0100

    Filtre d'authentification

--- server/src/main/java/fr/univlyon1/m1if/m1if03/servlets/Init.java
+++ server/src/main/java/fr/univlyon1/m1if/m1if03/servlets/Init.java
@@ -16,19 +16,6 @@ import java.util.HashMap;
 
 @WebServlet(name = "Init", urlPatterns = "/Init", loadOnStartup = 1)
 public class Init extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String login = request.getParameter("login");
-        if(login != null && !login.equals("")) {
-            User user = new User(login);
-            user.setNom(request.getParameter("nom"));
-            user.setAdmin(request.getParameter("admin") != null && request.getParameter("admin").equals("on"));
-            HttpSession session = request.getSession(true);
-            session.setAttribute("user", user);
-            response.sendRedirect("interface.jsp");
-        } else {
-            response.sendRedirect("index.jsp");
-        }
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.sendRedirect("index.jsp");
