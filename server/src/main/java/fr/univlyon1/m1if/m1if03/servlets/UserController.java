f1d3545b1221a234c3416076c94abcd740728466
jeremy <jeremy.thomas@etu.univ-lyon1.fr>
2020-11-20 18:04:29 +0100

    Contrôleurs de ressources

--- server/src/main/java/fr/univlyon1/m1if/m1if03/servlets/UserController.java
+++ server/src/main/java/fr/univlyon1/m1if/m1if03/servlets/UserController.java
@@ -1,21 +1,18 @@
 package fr.univlyon1.m1if.m1if03.servlets;
 
-import fr.univlyon1.m1if.m1if03.classes.Salle;
 import fr.univlyon1.m1if.m1if03.classes.User;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Map;
 
-@WebServlet(name = "UserController", urlPatterns = "/users/*")
+@WebServlet(name = "UserController", urlPatterns = "/user")
 public class UserController extends HttpServlet {
     Map<String, User> users;
 
@@ -25,15 +22,31 @@ public class UserController extends HttpServlet {
         this.users = (Map<String, User>) config.getServletContext().getAttribute("users");
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
         String contenu = request.getParameter("contenu");
-        if (contenu != null && contenu.equals("user")) {
-            request.setAttribute("user", users.get(request.getParameter("login")));
+        if (contenu != null) {
+            //GET sur un utilisateur particulier
+            if (contenu.equals("user")) {
+                if (request.getParameter("login") != null && !request.getParameter("login").equals(((User) (session.getAttribute("user"))).getLogin())) {
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                    return;
+                } else if (session.getAttribute("user") == null) {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                    return;
+                }
+                request.setAttribute("user", session.getAttribute("user"));
+                request.getRequestDispatcher("WEB-INF/jsp/interface.jsp").include(request, response);
+            }
+            //GET sur la collection d'utilisateurs
+            else if (contenu.equals("users")) {
+                request.setAttribute("users", users);
+                request.getRequestDispatcher("WEB-INF/jsp/interface_admin.jsp").include(request, response);
+            }
         }
-        request.getRequestDispatcher("WEB-INF/jsp/interface_admin.jsp").include(request, response);
     }
 }
