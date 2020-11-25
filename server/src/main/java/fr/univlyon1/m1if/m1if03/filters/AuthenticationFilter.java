6831073fcc202efda600ef5918b44a24cf4975fd
Julien Giraud <giraud740@gmail.com>
2020-11-26 00:05:28 +0100

    ajout filtre /salles et /passages

--- server/src/main/java/fr/univlyon1/m1if/m1if03/filters/AuthenticationFilter.java
+++ server/src/main/java/fr/univlyon1/m1if/m1if03/filters/AuthenticationFilter.java
@@ -23,32 +23,51 @@ public class AuthenticationFilter extends HttpFilter {
         this.users = (Map<String, User>) config.getServletContext().getAttribute("users");
     }
 
-    public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        // Test des URLs qui nécessitent une autorisation
-        if((req.getSession(false) == null || req.getSession(false).getAttribute("user") == null)) {
-            // Test d'une requête d'authentification
-            if(req.getMethod().equals("POST")) {
-                if(req.getParameter("action") != null && req.getParameter("action").equals("Connexion")
-                        && req.getParameter("login") != null && !req.getParameter("login").equals("")) {
-                    User user = new User(req.getParameter("login"));
-                    user.setNom(req.getParameter("nom"));
-                    user.setAdmin(req.getParameter("admin") != null && req.getParameter("admin").equals("on"));
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException {
+        HttpSession session = request.getSession();
+        User userSession = (User) session.getAttribute("user");
 
-                    // On ajoute l'user à la session
-                    HttpSession session = req.getSession(true);
-                    session.setAttribute("user", user);
-                    // On rajoute l'user dans le DAO
-
-                    users.put(req.getParameter("login"), user);
-                } else {
-                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-                    return;
-                }
-            } else {
-                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
+        // Filtre de /passages
+        if (request.getRequestURI().contains("/passages")) {
+            if (session == null || userSession == null) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 401
+            } else if (!request.getMethod().equals("POST") && !userSession.getAdmin()) {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN); // 403
             }
+            return;
         }
-        chain.doFilter(req, resp);
+
+        // Filtre de /salles
+        if (request.getRequestURI().contains("/passages")) {
+            if (session == null || userSession == null) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 401
+            } else if (!userSession.getAdmin()) {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN); // 403
+            }
+            return;
+        }
+
+        // Test d'une requête d'authentification
+        if (!request.getMethod().equals("POST")) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        if (request.getParameter("action") != null && request.getParameter("action").equals("Connexion")
+                && request.getParameter("login") != null && !request.getParameter("login").equals("")) {
+            User user = new User(request.getParameter("login"));
+            user.setNom(request.getParameter("nom"));
+            user.setAdmin(request.getParameter("admin") != null && request.getParameter("admin").equals("on"));
+
+            // On ajoute l'user à la session
+            session = request.getSession(true);
+            session.setAttribute("user", user);
+            // On rajoute l'user dans le DAO
+
+            users.put(request.getParameter("login"), user);
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
     }
+
 }
