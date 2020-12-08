2820ff3c56b96aad4061a0b7bd3a4ac804ae03f1
jeremy <jeremy.thomas@etu.univ-lyon1.fr>
2020-12-08 18:35:34 +0100

    mustache mon compte

--- client/js/render.js
+++ client/js/render.js
@@ -1,4 +1,13 @@
-function renderHello() {
+mockObjects = [
+  {
+    "nomSalle": "",
+    "capacite": -1,
+    "presents": 0,
+    "saturee": false
+  }
+]
+
+function render(scriptId, data, elementId) {
   var template = document.getElementById('template').innerHTML;
   var rendered = Mustache.render(template, { name: 'Luke' });
   document.getElementById('target').innerHTML = rendered;
