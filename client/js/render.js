88439bff2108632e310d3888a33e27bf416d3569
Julien Giraud <giraud740@gmail.com>
2020-12-08 18:58:50 +0100

    render

--- client/js/render.js
+++ client/js/render.js
@@ -1,14 +1,38 @@
-mockObjects = [
-  {
-    "nomSalle": "",
-    "capacite": -1,
-    "presents": 0,
-    "saturee": false
+const DATA = {
+  "salles": [
+  ],
+  "salle": null,
+  "passages": [
+    {
+      "login": "login",
+      "salle": "salle",
+      "entree": "entree",
+      "sortie": "sortie"
+    },
+    {
+      "login": "login",
+      "salle": "salle",
+      "entree": "entree",
+      "sortie": "sortie"
+    },
+  ],
+  "loggedUser": {
+    "login": "login",
+    "nom": "nom",
+    "admin": false
   }
-]
+}
 
-function render(scriptId, data, elementId) {
-  var template = document.getElementById('template').innerHTML;
-  var rendered = Mustache.render(template, { name: 'Luke' });
-  document.getElementById('target').innerHTML = rendered;
+function render() {
+  const templates = ["loggedUser", "passages"];
+  for (const template of templates) {
+    doRender(template);
+  }
+}
+
+function doRender(templateName) {
+  const template = document.getElementById(templateName).innerHTML;
+  const rendered = Mustache.render(template, { data: DATA[templateName] });
+  console.log(templateName, DATA[templateName]);
+  document.getElementById(templateName + 'Template').innerHTML = rendered;
 }
