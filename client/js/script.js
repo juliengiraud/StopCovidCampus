38628351f907a6dc0c068e2e936d39ecc207cec7
jeremy <jeremy.thomas@etu.univ-lyon1.fr>
2020-12-11 18:03:06 +0100

    Ajax pour mon compte et tous mes passages

--- client/js/script.js
+++ client/js/script.js
@@ -1,11 +1,8 @@
 $(document).ready(() => {
+    // Optimisation du chargement des templates en les pré-compilant
     $('*[id*=-template]').each(function() {
         Mustache.parse($(this).html());
     });
-    initEvents();
-})
-
-function initEvents() {
 
     // Déplier/replier le menu quand on clique sur son titre
     $('.navbar-brand').click(() => {
@@ -20,30 +17,75 @@ function initEvents() {
         switch (id) {
             case "mon-compte":
                 //AJAX vers /users/{userId}
-                DATA[id] = {
-                    "login": "login",
-                    "nom": "nom",
-                    "admin": false
-                };
+                $.ajax(
+                    {
+                        url: "https://192.168.75.76/api/v3/users/" + DATA["loggedUser"],
+                        success: function(result){
+                            DATA[id] = result;
+                        }
+                    }
+                );
+
                 render(id);
                 break;
 
             case "tous-mes-passages":
-                // AJAX GET vers /passages/byUser/{userId}
-                DATA[id] = [
+                /*
+                    AJAX GET vers /passages/byUser/{userId}, retourne un tableau qui contient l'ensemble des urls vers les passages de l'utilisateur de type :
+                    [
+                      "http://localhost:8080/passages/1",
+                      "http://localhost:8080/passages/5"
+                    ]
+                 */
+                let passagesUrl = [];
+                $.ajax(
                     {
-                        "login": "login",
-                        "salle": "salle",
-                        "entree": "entree",
-                        "sortie": "sortie"
-                    },
-                    {
-                        "login": "login",
-                        "salle": "salle",
-                        "entree": "entree",
-                        "sortie": "sortie"
-                    },
-                ];
+                        url: "https://192.168.75.76/api/v3/passages/byUser/" + DATA["loggedUser"],
+                        type: "GET",
+                        success: function(result, status){
+                            passagesUrl = result;
+                        },
+                        error: function(result, status, error) {
+                            $('#errMsg').html(result, status, error);
+                        },
+                        complete: function (result, status) {
+                            $('#errMsg').html(result, status);
+                        }
+                    }
+                );
+
+
+                const passages = [];
+
+                $.each(passagesUrl, (value) => {
+                    /*
+                        AJAX vers chaque passage de l'utilisateur par leur url. Renvoie un objet du type :
+                        {
+                          "user": "string",
+                          "salle": "string",
+                          "dateEntree": "string",
+                          "dateSortie": "string"
+                        }
+                     */
+                    $.ajax(
+                        {
+                            url: "https://192.168.75.76/api/v3/passages/" + value,
+                            type: "GET",
+                            success: function(result){
+                                passages.push(result);
+                            },
+                            error: function(result, status, error) {
+                                $('#errMsg').html(result, status, error);
+                            },
+                            complete: function (result, status) {
+                                $('#errMsg').html(result, status);
+                            }
+                        }
+                    );
+                });
+
+                DATA[id] = passages;
+
                 render(id);
                 break;
 
@@ -52,4 +94,4 @@ function initEvents() {
 
         $("#" + id).toggle("fast");
     });
-}
+});
