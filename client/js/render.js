2471a4ad06d5c8770e52491c0e866d7efdab304a
jeremy <jeremy.thomas@etu.univ-lyon1.fr>
2020-12-20 19:54:28 +0100

    pour https

--- client/js/render.js
+++ client/js/render.js
@@ -1,7 +1,6 @@
 let DATA = {};
 
 function render(id) {
-  console.log(id);
   const template = $("#" + id + "-template").html();
   const rendered = Mustache.render(template, { data: DATA[id] });
   $("#" + id + "-content").html(rendered);
